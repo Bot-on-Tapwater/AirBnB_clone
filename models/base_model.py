6ea3c2fde264fe642488a644f9335289a0e55d90
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-07-12 11:13:53 +0300

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,9 @@
 Create class BaseModel
 """
 
-# if __name__ == '__main__':
-
-
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     """
@@ -22,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -33,6 +32,7 @@ class BaseModel:
                         setattr(self, key, datetime_obj)
                     else:
                         setattr(self, key, value)
+        
     def __str__(self):
         """
         String representation
@@ -44,12 +44,13 @@ class BaseModel:
         Updates attr updated_at with current time
         """
         self.updated_at = datetime.now()
+        models.storage.save()
     
     def to_dict(self):
         """
         returns dictionary of key values of instance
         """
-        my_dict = self.__dict__
+        my_dict = self.__dict__.copy()
         # my_dict['id'] = self.id
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+Create class FileStorage
+"""
+from models.base_model import BaseModel
+import json
+
+class FileStorage:
+    """
+    Class FileStorage
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        returns the dictionary __objects
+        """
+        return self.__objects
+    
+    def new(self, obj):
+        """
+        sets in __objects the obj
+        """
+        class_name = obj.__class__.__name__ #obj is an instance of a class
+        self.__objects[f"{class_name}.{obj.id}"] = obj # __objects is a dictionary
+        # print(f"\n\t{self.__objects}\t")
+
+    def save(self):
+        """
+        serializes __objects to the JSON file
+        """
+        non_ser_dict = self.__objects # simply the __objects dict
+
+        # {'BaseModel.82e36784-150d-4ff2-80c5-82f884005231': <models.base_model.BaseModel object at 0x7f9ea87e6f10>}
+
+        # {"BaseModel.ee49c413-023a-4b49-bd28-f2936c95460d": {"my_number": 89, "__class__": "BaseModel", "updated_at": "2017-09-28T21:07:25.047381", "created_at": "2017-09-28T21:07:25.047372", "name": "My_First_Model", "id": "ee49c413-023a-4b49-bd28-f2936c95460d"}}
+
+        ser_dict = {}
+        for obj_id in non_ser_dict.keys():
+            ser_dict[obj_id] = non_ser_dict[obj_id ].to_dict()
+        
+        with open(self.__file_path, "w") as json_file:
+            json.dump(ser_dict, json_file)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        """
+        try:
+            with open(self.__file_path) as json_file:
+                ser_dict = json.load(json_file)
+                for values in ser_dict.values():
+                    cls_name = values["__class__"]
+                    del values["__class__"]
+                    self.new(eval(cls_name)(**values))
+
+                    # BaseModel(**kwargs)
+
+        except FileNotFoundError:
+            return
