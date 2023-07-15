e189f81d7116d68793747a1632b9ecec296ddb1f
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-07-15 22:18:54 +0300

    Change the doc strings in models.init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Takes care of storage requirements"""
-
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
